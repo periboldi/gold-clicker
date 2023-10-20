d5ec69ca345adf1a9485cb429899850e3c8c2be6
periboldi <boldizsar.pereznyak@gmail.com>
2023-10-20 08:26:45 +0200

    JS v4 ár formázása

--- js/clicker.js
+++ js/clicker.js
@@ -26,6 +26,15 @@ let price = 10;
 let link = "./images/prospecting.png"; 
 */
 
+/* PRE: 0 <= Price <= 999999 */ 
+// PRE = előfeltétel
+function formatPrice(price) {
+    if (price < 1000) return price;
+    let kValue = price / 1000;
+    return `${kValue}K`
+
+}
+
 let skillList = [ 
     {
     skillName: 'ARANYKUTATÁS',
@@ -37,42 +46,42 @@ let skillList = [
     },
     {
     skillName: 'BAGOLYIDOMÍTÁS',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 10,
     description: "Baglyok betanítását készpénzre válthatjuk. Magasabb szinten postabaglyokat is nevelhetünk.",
     amount: 0,
-    price: 10,
+    price: 200,
     link: "./images/owl.png",
     },
     {
     skillName: 'GYÓGYFŐZET',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 25,
     description: "Minél jobban kitanuljuk a gyógyfőzet készítését, annál több gyógyfőzetet tudunk értékesíteni aranyért cserébe.",
     amount: 0,
-    price: 10,
+    price: 750,
     link: "./images/potion.png",
     },
     {
     skillName: 'KERESKEDELEM',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 100,
     description: "Varázstárgyak készítésével és értékesítésével profitot zsebelhetünk be.",
     amount: 0,
-    price: 10,
+    price: 4000,
     link: "./images/trade.png",
     },
     {
-    skillName: 'Alkímia',
-    goldPerClickIncrement: 1,
+    skillName: 'ALKÍMIA',
+    goldPerClickIncrement: 300,
     description: "Az aranykészítés tudománya titkos recept alapján.",
     amount: 0,
-    price: 10,
+    price: 15000,
     link: "./images/alchemy.png",
     },
     {
     skillName: 'VARÁZSTUDOMÁNY',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 1000,
     description: "Az alkímia hatását tovább erősíti és oktatási tevékenységet is végezhetünk.",
     amount: 0,
-    price: 10,
+    price: 100000,
     link: "./images/wizardry.png",
     },
 
@@ -89,7 +98,7 @@ function getSkill({ skillName, goldPerClickIncrement, description, amount, price
             </td>
             <td class="upgrade-stats-cell">
                 <p>db: ${amount}</p>
-                <p>ár: ${price}</p>
+                <p>ár: ${formatPrice(price)}</p>
             </td>
             <td>
                 <img src="${link}" alt="${skillName}" >
@@ -97,14 +106,14 @@ function getSkill({ skillName, goldPerClickIncrement, description, amount, price
         </tr>
     `;
 }
-
+/*
 let skillTemplate = getSkill(skillList[0]);
 let skillTemplate1 = getSkill(skillList[1]);
 let skillTemplate2 = getSkill(skillList[2]);
 let skillTemplate3 = getSkill(skillList[3]);
 let skillTemplate4 = getSkill(skillList[4]);
 let skillTemplate5 = getSkill(skillList[5]);
-/* let skillTemplateAll = getSkill(skillList);
+let skillTemplateAll = getSkill(skillList);
 
 document.querySelector(".js-skills-tbody").innerHTML = skillTemplateAll;
 document.querySelector(".js-skills-tbody").innerHTML = skillTemplate1;
@@ -128,7 +137,6 @@ document.querySelector(".js-skills-tbody").innerHTML = skillTable;
 
 
 
-
 // ********************* EMPLOYEES RÉSZ KEZDETE **************************
 
 let employeeList = [ 
@@ -137,47 +145,47 @@ let employeeList = [
     goldPerClickIncrement: 1,
     description: "Aranyat keres és talál.",
     amount: 0,
-    price: 10,
+    price: 100,
     link: "./images/prospector.png",
     },
     {
     employeeName: 'BAGOLYIDOMÁR',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 5,
     description: "Szerződéses munkatársként baglyokat tanít.",
     amount: 0,
-    price: 10,
+    price: 1000,
     link: "./images/owl_trainer.png",
     },
     {
     employeeName: 'GYÓGYFŐZETKÉSZÍTŐ',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 10,
     description: "Gyógyfőzeteket készít, amiért aranyat kap.",
     amount: 0,
-    price: 10,
+    price: 3000,
     link: "./images/potion-mixer.png",
     },
     {
     employeeName: 'KERESKEDŐ',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 25,
     description: "Varázstárgyakat ad el.",
     amount: 0,
-    price: 10,
+    price: 10000,
     link: "./images/trader.png",
     },
     {
     employeeName: 'VARÁZSLÓPROFESSZOR',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 100,
     description: "Tanulókat képez ki szerződéses munkatársként. Szabadidejében alkímiával foglalkozik.",
     amount: 0,
-    price: 10,
+    price: 50000,
     link: "./images/alchemist.png",
     },
     {
     employeeName: 'BEFEKTETŐ KACSA',
-    goldPerClickIncrement: 1,
+    goldPerClickIncrement: 1000,
     description: "Dagobeert bácsihoz hasonló szakértelemmel kezeli és fiataltatja a vagyonodat.",
     amount: 0,
-    price: 10,
+    price: 250000,
     link: "./images/gold_duck.png",
     },
 
@@ -193,7 +201,7 @@ function getEmployee({ employeeName, goldPerClickIncrement, description, amount,
                 </td>
                 <td class="upgrade-stats-cell">
                     <p>db: ${amount}</p>
-                    <p>ár: ${price}</p>
+                    <p>ár: ${formatPrice(price)}</p>
                 </td>
                 <td>
                     <p><strong>${employeeName} (${ goldPerClickIncrement } arany / klikk)</strong></p>
@@ -224,4 +232,7 @@ for (i = 0; i <= (employeeList.length - 1); i += 1) {
 }
 
 document.querySelector(".js-employees-tbody").innerHTML = employeeTable;
+
+// debugger;
+
 
